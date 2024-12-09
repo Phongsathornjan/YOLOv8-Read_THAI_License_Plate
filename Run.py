0683525999224674fa28b6cacf5a03a33ff9ac8c
phongsathornjanjamsai@gmail.com <phongsathornjanjamsai@gmail.com>
2024-12-09 10:02:57 +0700

    sort xy

--- Run.py
+++ Run.py
@@ -27,17 +27,36 @@ def plot_license_plate(results, model):
     
     return all_boxes  # ส่งคืนพิกัดของทุกกรอบ
 
-def read_license_plate(results, model,i):
+def read_license_plate(results, model, i):
+    xy = []  # ใช้ list เพื่อเก็บข้อมูล box ทั้งหมด
     for result in results:
         print(f"License Plate {i}")
-        for box in result.boxes:            
-            # ดึงชื่อคลาสจาก index
-            class_index = int(box.cls[0])  # ต้องแปลงเป็น int ก่อน
-            class_name = model.names[class_index]  # ดึงชื่อคลาส
+        if(len(result.boxes.xyxy) != 0):
+            for box in result.boxes:
+                # ดึงข้อมูลของ bounding box
+                x1, y1, x2, y2 = box.xyxy[0].tolist()
+                class_index = int(box.cls[0])  # ดึง index ของ class
+                class_name = model.names[class_index]  # ดึงชื่อคลาส
+                confidence = float(box.conf[0])  # ความมั่นใจ
+                
+                # เพิ่มข้อมูล box ลงใน list
+                xy.append({
+                    "coordinates": [x1, y1, x2, y2],
+                    "class_name": class_name,
+                    "confidence": confidence
+                })
+            # จัดเรียง box ตามค่า y1
+            sorted_boxes = sorted(xy, key=lambda ob: ob["coordinates"][1])  # ใช้ y1
+            provide = sorted_boxes[len(sorted_boxes)-1]
+            sorted_boxes.pop(len(sorted_boxes)-1)
+            final_sorted_boxes = sorted(sorted_boxes , key=lambda ob: ob["coordinates"][0])  # ใช้ x1
             
-            # พิมพ์ความมั่นใจและชื่อคลาส
-            print(f"Confidence: {box.conf[0]:.2f}, Class Name: {class_name}")
-            
+            for result in final_sorted_boxes:
+                print(result["class_name"])
+                
+            print(provide["class_name"])
+        else:
+            print("Can't detect label in this License Plate")
 
 # โหลดโมเดล
 Crop_License_Plate_model = YOLO("train_Crop_License_Plate/train/weights/best.pt")
